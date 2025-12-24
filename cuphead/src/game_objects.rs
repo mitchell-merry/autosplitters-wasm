ce899f5cb27e7255fd786a76f5f824c9e37c5f73
Mitchell Merry <mitchellmerry145@gmail.com>
2025-12-24 13:48:51 +1100

    use asr refactors...

--- cuphead/src/game_objects.rs
+++ cuphead/src/game_objects.rs
@@ -1,10 +1,30 @@
 use asr::game_engine::unity::scene_manager;
-use asr::game_engine::unity::scene_manager::CppGameObject;
+use asr::game_engine::unity::scene_manager::{CppGameObject, Scene, SceneManager};
 use asr::Process;
 use helpers::error::SimpleError;
 use helpers::watchers::{ValueGetter, Watcher};
 use std::cell::Cell;
 use std::error::Error;
+
+fn get_scene_if_active(
+    process: &Process,
+    scene_manager: &SceneManager,
+    scene: &str,
+) -> Result<Scene, Box<dyn Error>> {
+    let active_scene = scene_manager
+        .get_current_scene(process)
+        .map_err(|_| SimpleError::from("failed to get current scene"))?;
+
+    let active_scene_name = active_scene
+        .name(process, scene_manager)
+        .map_err(|_| SimpleError::from("failed reading game object"))?;
+
+    if scene != active_scene_name {
+        return Err(SimpleError::from(&format!("unable to get game object path, in scene {active_scene_name} while expected scene was {}", scene)).into());
+    }
+
+    Ok(active_scene)
+}
 
 pub struct GameObjectActivePath<'a> {
     process: &'a Process,
@@ -38,41 +58,28 @@ impl<'a> GameObjectActivePath<'a> {
 
 impl<'a> ValueGetter<bool> for GameObjectActivePath<'a> {
     fn get(&self) -> Result<bool, Box<dyn Error>> {
-        let active_scene = self
-            .scene_manager
-            .get_current_scene(self.process)
-            .map_err(|_| SimpleError::from("failed to get current scene"))?;
-
-        let active_scene_name = active_scene
-            .name(self.process, self.scene_manager)
-            .map_err(|_| SimpleError::from("failed reading game object"))?;
-
-        if self.scene != active_scene_name {
-            self.cached_object.set(None);
-
-            return Err(SimpleError::from(&format!("unable to get game object path, in scene {active_scene_name} while expected scene was {}", self.scene)).into());
-        }
+        let active_scene = get_scene_if_active(self.process, self.scene_manager, self.scene)
+            .map_err(|e| {
+                self.cached_object.set(None);
+                e
+            })?;
 
         // this is pretty jank, but we're using the cached address if one exists
         let game_object = match self.cached_object.take() {
             Some(game_object) => game_object,
             None => {
-                let mut current_transform = self
-                    .scene_manager
-                    .get_root_game_object(self.process, self.root_object_name)
-                    .map_err(|_| SimpleError::from("couldnt find root object"))?;
+                let transform = active_scene
+                    .find_transform(
+                        self.process,
+                        self.scene_manager,
+                        self.root_object_name,
+                        self.path,
+                    )
+                    .map_err(|_| SimpleError::from("couldnt find transform"))?;
 
-                for object_name in self.path {
-                    current_transform = current_transform
-                        .get_child(self.process, self.scene_manager, object_name)
-                        .map_err(|_| SimpleError::from("could not find the child"))?;
-                }
-
-                let game_object = current_transform
+                transform
                     .get_game_object(self.process, self.scene_manager)
-                    .map_err(|_| SimpleError::from("couldnt get game_object"))?;
-
-                game_object
+                    .map_err(|_| SimpleError::from("couldnt get game_object"))?
             }
         };
 
@@ -89,3 +96,17 @@ impl<'a> From<GameObjectActivePath<'a>> for Watcher<'a, bool> {
         Watcher::new(Box::new(value))
     }
 }
+
+// pub struct MonoBehaviourFieldPath<'a, T: CheckedBitPattern> {
+//     _phantom: std::marker::PhantomData<T>,
+//     process: &'a Process,
+//     scene_manager: &'a scene_manager::SceneManager,
+//
+//     scene: &'static str,
+//     root_object_name: &'static str,
+//     game_object_path: &'static [&'static str],
+//     component_name: &'static str,
+//     field_path: &'static [&'static str],
+//
+//     cached_addr: Cell<Option<CppGameObject>>,
+// }
